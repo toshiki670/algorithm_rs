b3250b14c46e38281c93511319fc8b259ef9be12
Toshiki <toshiki.dev@protonmail.ch>
2021-05-13 19:05:20 +0000

    n_triangle algorithm 'feature/n_triangle'

---
+++ src/n_triangle.rs
@@ -0,0 +1,37 @@
+use proconio::input;
+
+pub fn exec() {
+  input! {
+    n : u32,
+  }
+
+  println!("{}", calc(n));
+}
+
+
+fn calc(n: u32) -> u64 {
+  let mut sum: u64 = 0;
+
+  for i in 1..=n {
+    // small triangles
+    sum += i as u64;
+
+    // small invertted triangles
+    sum += i as u64 - 1;
+
+    // Triangles with height greater than or equal to 2
+    for j in 1..=(i - 1) {
+      sum += j as u64;
+    }
+
+    // Invertted triangles with height greater than or equal to 2
+    if 4 <= i {
+      for j in (4..=i).filter(|x| x % 2 == 0) {
+        sum += (i - j) as u64 + 1;
+      }
+    }
+
+    // dbg!(sum);
+  }
+  sum
+}
