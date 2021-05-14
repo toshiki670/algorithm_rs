1c9bba27e8babb6264884ac63cf19cfd6383c679
Toshiki <toshiki.dev@protonmail.ch>
2021-05-14 15:57:11 +0000

    Fix indent 'feature/n_triangle'

--- src/n_triangle.rs
+++ src/n_triangle.rs
@@ -1,43 +1,43 @@
 use proconio::input;
 
 pub fn exec() {
-  input! {
-    n : u32,
-  }
+    input! {
+        n : u32,
+    }
 
-  println!("{} is {}.", n, calc(n));
+    println!("{} is {}.", n, calc(n));
 }
 
 
 fn calc(n: u32) -> u64 {
-  let mut sum: u64 = 0;
+    let mut sum: u64 = 0;
 
-  if n <= 0 {return 0};
+    if n <= 0 {return 0};
 
-  // small triangles
-  sum += (n * (n + 1) / 2) as u64;
-  dbg!(sum);
-
-  // small invertted triangles
-  sum += ((n - 1) * n / 2) as u64;
-  dbg!(sum);
-
-  for i in 2..=n {
-    // Triangles with height greater than or equal to 2
-    for j in 1..=(i - 1) {
-      sum += j as u64;
-    }
-
-    // Invertted triangles with height greater than or equal to 2
-    if 4 <= i {
-      for j in (4..=i).filter(|x| x % 2 == 0) {
-        sum += (i - j) as u64 + 1;
-      }
-    }
-
+    // small triangles
+    sum += (n * (n + 1) / 2) as u64;
     dbg!(sum);
-  }
-  sum
+
+    // small invertted triangles
+    sum += ((n - 1) * n / 2) as u64;
+    dbg!(sum);
+
+    for i in 2..=n {
+        // Triangles with height greater than or equal to 2
+        for j in 1..=(i - 1) {
+            sum += j as u64;
+        }
+
+        // Invertted triangles with height greater than or equal to 2
+        if 4 <= i {
+            for j in (4..=i).filter(|x| x % 2 == 0) {
+                sum += (i - j) as u64 + 1;
+            }
+        }
+
+        dbg!(sum);
+    }
+    sum
 }
 
 #[cfg(test)]
