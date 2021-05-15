6ad55c642f18fb97c2bc8d42a40d1af1d5b3a345
Toshiki <toshiki.dev@protonmail.ch>
2021-05-15 10:56:17 +0000

    Delete comment 'feature/n_triangle_fix'

--- src/n_triangle.rs
+++ src/n_triangle.rs
@@ -67,12 +67,6 @@ fn sum_arith_progression(first: u64, diff: u64, length: u64) -> u64 {
     length * (2 * first + (length - 1) * diff) / 2
 }
 
-// 等差数列 を求める関数
-// 初項 a，公差 d の等差数列 an の一般項は
-// an = a + (n − 1)d
-// fn arith_progression(first: u64, diff: u64, n: u64) -> u64 {
-//     first + (n - 1) * diff
-// }
 
 #[cfg(test)]
 mod tests {
