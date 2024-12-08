525cf7cce209b0e7601031a32c724e558eb78be3
Toshiki <bushy.trivial.0o@icloud.com>
2024-12-08 22:51:17 +0900

    Add CLI module for command-line interface handling
    - Introduced a new `cli.rs` file to encapsulate command-line argument parsing and routing.
    - Implemented `Cli` struct with subcommands for Base64 and N-Triangle functionalities.
    - Refactored `main.rs` to utilize the new CLI module, simplifying the main function and improving code organization.
    - Integrated logging functionality based on verbosity flag.

---
+++ src/cli.rs
@@ -0,0 +1,45 @@
+use std::env;
+
+use clap::{Parser, Subcommand};
+use env_logger;
+use log::Level;
+
+use crate::{base64, n_triangle};
+
+pub trait CliRoute {
+    fn route(&self);
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about = "Algorithm tool", long_about = None)]
+pub struct Cli {
+    #[arg(short, long, help = "Show logs")]
+    pub verbose: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Base64(base64::Base64),
+    NTriangle(n_triangle::NTriangle),
+}
+
+impl CliRoute for Cli {
+    fn route(&self) {
+        if self.verbose {
+            env::set_var("RUST_LOG", Level::Trace.to_string());
+        }
+        env_logger::init();
+
+        match &self.command {
+            Commands::Base64(cli) => {
+                base64::exec(cli);
+            }
+            Commands::NTriangle(cli) => {
+                n_triangle::exec(cli);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,10 @@
-use std::env;
-
-use clap::{Parser, Subcommand};
-use env_logger;
-use log::Level;
+use clap::Parser;
+use cli::{Cli, CliRoute};
 
 mod base64;
+mod cli;
 mod n_triangle;
 
-
-#[derive(Parser, Debug)]
-#[command(author, version, about = "Algorithm tool", long_about = None)]
-pub struct Cli {
-    #[arg(short, long, help = "Show logs")]
-    pub verbose: bool,
-
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand, Debug)]
-enum Commands {
-    Base64(base64::Base64),
-    NTriangle(n_triangle::NTriangle),
-}
-
 fn main() {
-    // let matches = cli().get_matches();
-    let cli = Cli::parse();
-
-    if cli.verbose {
-        env::set_var("RUST_LOG", Level::Trace.to_string());
-    }
-    env_logger::init();
-
-    match &cli.command {
-        Commands::Base64(cli) => {
-            base64::exec(cli);
-        },
-        Commands::NTriangle(cli) => {
-            n_triangle::exec(cli);
-        },
-    }
-
-    println!("\nGood bye!");
+    Cli::parse().route();
 }
